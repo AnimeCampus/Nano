7a27437575e5b886a3610f99192c4d98b156d849
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-05 22:43:38 +0530

    Update nano.py

--- nano.py
+++ nano.py
@@ -2,11 +2,11 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # Set your bot token here
-TOKEN = 'YOUR_BOT_TOKEN'
+TOKEN = '6206599982:AAG374J4c9eb0v2-3cbDMq4yjj97PuMIyB0'
 
 # Replace 'YOUR_API_ID' and 'YOUR_API_HASH' with your own API credentials
-API_ID = 'YOUR_API_ID'
-API_HASH = 'YOUR_API_HASH'
+API_ID = '16743442'
+API_HASH = '12bbd720f4097ba7713c5e40a11dfd2a'
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 
@@ -94,4 +94,5 @@ def inline_command(client, message):
 def handle_callback(client, callback_query):
     data = callback_query.data
     message = callback_query.message
-   
+    if data == "btn1":
+        client.send_message(chat_id=message.chat.id, text="You clicked Button button 1") 
