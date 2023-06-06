d42495470bb0716d2deedac5e576041698179d4b
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-06 08:05:59 +0530

    Update nano.py

--- nano.py
+++ nano.py
@@ -62,102 +62,7 @@ def help_command(client, message):
 
     client.send_message(chat_id=message.chat.id, text=help_text, reply_markup=reply_markup)
 
-    
-  
-import os
 
-import sys
-
-from os import environ, execle, system
-
-from git import Repo
-
-from git.exc import InvalidGitRepositoryError
-
-from pyrogram import Client, filters
-
-from pyrogram.types import Message
-
-# Initialize your Pyrogram client
-
-app = Client("my_bot")
-
-# Replace 'YOUR_ADMINS_LIST' with a list of user IDs of the bot admins
-
-ADMINS = [6198858059]
-
-UPSTREAM_REPO = "https://github.com/PyroUserBot/Nano"
-
-def gen_chlog(repo, diff):
-
-    upstream_repo_url = Repo().remotes[0].config_reader.get("url").replace(".git", "")
-
-    ac_br = repo.active_branch.name
-
-    ch_log = ""
-
-    tldr_log = ""
-
-    ch = f"<b>updates for <a href={upstream_repo_url}/tree/{ac_br}>[{ac_br}]</a>:</b>"
-
-    ch_tl = f"updates for {ac_br}:"
-
-    d_form = "%d/%m/%y || %H:%M"
-
-    for c in repo.iter_commits(diff):
-
-        ch_log += (
-
-            f"\n\n💬 <b>{c.count()}</b> 🗓 <b>[{c.committed_datetime.strftime(d_form)}]</b>\n<b>"
-
-            f"<a href={upstream_repo_url.rstrip('/')}/commit/{c}>[{c.summary}]</a></b> 👨‍💻 <code>{c.author}</code>"
-
-        )
-
-        tldr_log += f"\n\n💬 {c.count()} 🗓 [{c.committed_datetime.strftime(d_form)}]\n[{c.summary}] 👨‍💻 {c.author}"
-
-    if ch_log:
-
-        return str(ch + ch_log), str(ch_tl + tldr_log)
-
-    return ch_log, tldr_log
-def updater():
-    try:
-        repo = Repo()
-
-    except InvalidGitRepositoryError:
-
-        repo = Repo.init()
-        origin = repo.create_remote("upstream", UPSTREAM_REPO)
-        origin.fetch()
-        repo.create_head("master", origin.refs.master)
-        repo.heads.master.set_tracking_branch(origin.refs.master)
-        repo.heads.master.checkout(True)
-    ac_br = repo.active_branch.name
-    if "upstream" in repo.remotes:
-        ups_rem = repo.remote("upstream")
-    else:
-        ups_rem = repo.create_remote("upstream", UPSTREAM_REPO)
-    ups_rem.fetch(ac_br)
-    changelog, tl_chnglog = gen_chlog(repo, f"HEAD..upstream/{ac_br}")
-    return bool(changelog)
-@app.on_message(filters.command("update") & filters.user(ADMINS))
-async def update_bot(_, message: Message):
-    chat_id = message.chat.id
-    msg = await message.reply_text("Checking updates...")
-    update_avail = updater()
-    if update_avail:
-        await msg.edit("✅ Update finished!")
-        system("git pull -f && pip3 install --no-cache-dir -r requirements.txt")
-        execle(sys.executable, sys.executable, "nano.py", environ)
-        return
-    await msg.edit(
-        f"Bot is **up-to-date** with branch [master]({UPSTREAM_REPO}/tree/master)",
-        disable_web_page_preview=True,
-    )
- 
-
-  
 @app.on_message(filters.command("echo"))
 def echo_command(client, message):
     # Remove the command from the message text
@@ -241,7 +146,9 @@ def handle_callback(client, callback_query):
     if data == "btn4":
         client.send_message(chat_id=message.chat.id, text="You clicked button 4") 
         
+        
 app.run()
+print("bot started....")
 idle()
        
     
