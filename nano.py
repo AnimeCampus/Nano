1b235001a4be16ab64631bfdfb924298c96985db
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-06 23:55:13 +0530

    Update nano.py

--- nano.py
+++ nano.py
@@ -1,56 +1,16 @@
-import os
-
 import requests
-
 from pyrogram import Client, filters, idle
 
-# Initialize the Pyrogram client
+# Create a Pyrogram Client
+app = Client("6206599982:AAEtRoU2jV7heQn8t0Zkwh1L6khiC8EXfcM")
 
-api_id = "16743442"
+@app.on_message(filters.command("news"))
+def news(_, message):
+    url = 'https://newsapi.org/v2/top-headlines?country=IN&apiKey=6b32967165ca4a68a88fe7eef9160092'
+    response = requests.get(url).json()
+    headlines = [article['title'] for article in response['articles']]
+    message.reply_text('\n'.join(headlines))
 
-api_hash = "12bbd720f4097ba7713c5e40a11dfd2a"
-
-bot_token = "6206599982:AAEtRoU2jV7heQn8t0Zkwh1L6khiC8EXfcM"
-
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-
-# Handler for the /start command
-
-@app.on_message(filters.command("start"))
-
-def start(client, message):
-
-    client.send_message(chat_id=message.chat.id, text="Hello! Send me a keyword to search for a GIF.")
-
-# Handler for text messages
-
-@app.on_message(filters.private & filters.text)
-
-def handle_message(client, message):
-
-    keyword = message.text
-
-    gif_url = search_gif(keyword)
-
-    client.send_animation(chat_id=message.chat.id, animation=gif_url)
-
-# Search for a GIF using the Giphy API
-
-def search_gif(keyword):
-
-    giphy_api_key = "WBJ2Bzj0srZLaewLYf5ulkhBTjPJAYYU"
-
-    url = f"https://api.giphy.com/v1/gifs/random?api_key={giphy_api_key}&tag={keyword}"
-
-    response = requests.get(url)
-
-    data = response.json()
-
-    gif_url = data["data"].get("image_original_url", "Default URL")
-
-    return gif_url
-
-# Start the bot
-
+# Start the Pyrogram Client
 app.run()
 idle()
