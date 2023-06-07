9b33d322ae531ebf0f22057af05e55a1c9c1f2b3
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-07 22:35:45 +0530

    Delete Config.py

--- Config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    # Set your bot token here
-    TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
-
-    # Replace 'YOUR_API_ID' and 'YOUR_API_HASH' with your own API credentials
-    API_ID = 'YOUR_API_ID'
-    API_HASH = 'YOUR_API_HASH'
